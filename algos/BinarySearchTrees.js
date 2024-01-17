c0b5b6ca4b8e7cc7f62f96fe1e6c3fb8f5c50349
Spencer Rauch <rauchs@gmail.com>
2024-01-17 05:22:49 -0800

    test case

--- algos/BinarySearchTrees.js
+++ algos/BinarySearchTrees.js
@@ -214,4 +214,35 @@ threeLevelTree.root.right.left = new BSTNode(13);
 
 
 
-//test 2S
+// /***************** Uncomment after insert method is created. ****************/
+// const fullTree = new BinarySearchTree();
+// fullTree
+//     .insert(25)
+//     .insert(15)
+//     .insert(10)
+//     .insert(22)
+//     .insert(4)
+//     .insert(12)
+//     .insert(18)
+//     .insert(24)
+//     .insert(50)
+//     .insert(35)
+//     .insert(70)
+//     .insert(31)
+//     .insert(44)
+//     .insert(66)
+//     .insert(90);
+
+// /* fullTree
+//                     root
+//                 <-- 25 -->
+//               /            \
+//             15             50
+//           /    \         /    \
+//         10     22      35     70
+//       /   \   /  \    /  \   /  \
+//     4    12  18  24  31  44 66  90
+
+// */
+
+// fullTree.print();
