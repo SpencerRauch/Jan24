4bf3682e47bd837a1dd6d63a14b06d31dda85b47
Spencer Rauch <rauchs@gmail.com>
2024-01-26 06:34:44 -0800

    DLLs 2

--- FirstWeb/obj/Debug/net8.0/FirstWeb.AssemblyInfo.cs
+++ FirstWeb/obj/Debug/net8.0/FirstWeb.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("FirstWeb")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c11c9b9614e5c80942b026977f6d5f40a44c1d17")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+fc99a7163593825116d6bd672e67a7658a163daa")]
 [assembly: System.Reflection.AssemblyProductAttribute("FirstWeb")]
 [assembly: System.Reflection.AssemblyTitleAttribute("FirstWeb")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
--- algos/DoublyLinkedLists.js
+++ algos/DoublyLinkedLists.js
@@ -149,6 +149,32 @@ class DoublyLinkedList {
         items.forEach((item) => this.insertAtBack(item));
         return this;
     }
+
+    /**
+ * Inserts a new node with the given newVal after the node that has the
+ * given targetVal as it's data.
+ * - Time: O(?).
+ * - Space: O(?).
+ * @param {any} targetVal The node data to find.
+ * @param {any} newVal Data for the new node.
+ * @returns {boolean} Indicates if the new node was added.
+ */
+    insertAfter(targetVal, newVal) {
+        //your code here
+    }
+
+    /**
+     * Inserts a new node with the given newVal before the node that has the
+     * given targetVal as it's data.
+     * - Time: O(?).
+     * - Space: O(?).
+     * @param {any} targetVal The node data to find.
+     * @param {any} newVal Data for the new node.
+     * @returns {boolean} Indicates if the new node was added.
+     */
+    insertBefore(targetVal, newVal) {
+        //your code here
+    }
 }
 const emptyList = new DoublyLinkedList();
 
